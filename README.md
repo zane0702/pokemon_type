6a8f98054c39676132e1f9eb054a4c0a475cc3c6
zane0702 <so1rou72@gmail.com>
2023-01-14 15:50:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# flask-mnist-app
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# zane_mnist
+# pokemon_type
+# pokemon_type
---
+++ pokemon.py
@@ -0,0 +1,56 @@
+import os
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing import image
+
+import numpy as np
+
+
+classes=["エスパータイプ","ゴーストタイプ","ドラゴンタイプ","ノーマルタイプ","フェアリータイプ","地面タイプ","岩タイプ","悪タイプ","格闘タイプ","毒タイプ","水タイプ","氷タイプ","炎タイプ","草タイプ","虫タイプ","鋼タイプ","電気タイプ","飛行タイプ"]
+image_size = 100
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./model_pokemon_18type.h5')#学習済みモデルをロード
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            #受け取った画像を読み込み、np形式に変換
+            img = image.load_img(filepath, target_size=(image_size,image_size))
+            img = image.img_to_array(img)
+            data = np.array([img])
+            #変換したデータをモデルに渡して予測する
+            result = model.predict(data)[0]
+            predicted = result.argmax()
+            pred_answer = "これは " + classes[predicted] + " です"
+
+            return render_template("index.html",answer=pred_answer)
+
+    return render_template("index.html",answer="")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
+
